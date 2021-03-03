d43d24299a46d3dfdafff17bff8464d354b756bc
Vanessa A. (she/her/BE) <69529052+nessaarruda@users.noreply.github.com>
2021-03-03 08:57:44 -0700

    Update search_controller.rb

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,7 @@ class SearchController < ApplicationController
   def index
     @station = StationFacade.get_a_station
     conn2 = Faraday.new('http://open.mapquestapi.com')
-    response2 = conn2.get("/directions/v2/route?key=C001dcSUYX2coaBMlAb01XAk9UItVvCl&from=1331 17th St, Denver, CO&to=1800 Wazee Stree, Denver, CO")
+    response2 = conn2.get("/directions/v2/route?key={key}from=1331 17th St, Denver, CO&to=1800 Wazee Stree, Denver, CO")
     parsed2 = JSON.parse(response2.body, symbolize_names: true)
     route = parsed2[:route][:legs][0][:maneuvers][1]
     @directions = route[:narrative]
